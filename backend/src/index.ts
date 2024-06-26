4f5d5986ece7570ae36628805b699f5c6afb5de8
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-06-26 11:08:26 +0500

    backend initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  res.send('Hello zunnoorain');
+});
+
+app.listen(4000, () => console.log(`Server is running on PORT: ${4000}`));
