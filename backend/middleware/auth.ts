59bfe3ed2a0289a8ce786d825e7bd5a735218b3a
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-06-28 09:43:49 +0500

    jwtParser middleware created

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -1,7 +1,56 @@
+import { NextFunction, Request, Response } from 'express';
 import { auth } from 'express-oauth2-jwt-bearer';
+import jwt from 'jsonwebtoken';
+import { pool } from '../lib/db';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: 'RS256',
 });
+
+export const jwtParser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(400).json({ message: 'Unauthorized' });
+  }
+
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    return res.status(400).json({ message: 'Token not found' });
+  }
+
+  const db = await pool.connect();
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decoded.sub;
+    const { rows: user } = await db.query(
+      `SELECT * FROM users WHERE auth0Id=1`,
+      [auth0Id]
+    );
+    if (!user[0]) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+    req.userId = user[0].id;
+    req.auth0Id = auth0Id as string;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'Unauthorized' });
+  } finally {
+    db.release();
+  }
+};
