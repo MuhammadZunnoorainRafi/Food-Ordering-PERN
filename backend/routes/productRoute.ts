5ad7a78f51d93e8b6184009f0656bd1a049b171b
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-07-01 10:59:25 +0500

    product created successfully with menu_items type JSONB

--- backend/routes/productRoute.ts
+++ backend/routes/productRoute.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { createProductController } from '../controllers/productController';
 
 const productRoute = express.Router();
+
+productRoute.post('/', createProductController);
 
 export default productRoute;
